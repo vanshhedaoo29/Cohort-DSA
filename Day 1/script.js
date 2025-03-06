be9d55aeeb2de0614357b4bedda2f0f74dfbd143
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-06 22:12:26 +0530

    DSA Day 1...

---
+++ Day 1/script.js
@@ -0,0 +1,102 @@
+// Sum of two integers
+
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+
+/*
+string + string = string (concatenation) 
+string + int = string (concatenation)
+int + int = int (Arithmetic)
+*/
+
+// let a = "Hello";             string + string = string (concatenation) 
+// let b = "World";
+// console.log(a + b);
+// console.log(typeof(a + b));
+
+// let a = "Hello";
+// let b = 10;                             string + int = string (concatenation)
+// console.log(a + b);
+// console.log(typeof(a + b));
+
+// let a = 10;
+// let b = 20;
+// console.log(a + b);              int + int = int (Arithmetic)
+// console.log(typeof(a + b));
+
+
+// let a = 10;
+// let b = 20;
+
+// console.log("Sum of "+ a +" and "+ b +" is "+ a+b);    //Sum of 10 and 20 is 1020
+// console.log("Sum of "+ a +" and "+ b +" is "+ (a+b));  // Sum of 10 and 20 is 30
+// console.log(a+b +" is the sum of "+ a +" and "+ b );   //30 is the sum of 10 and 20
+
+// Type Coersion
+// console.log(1+1);         // 2     Number
+// console.log(1+"1");       // 11    String
+// console.log(1-1);         // 0     Number
+// console.log(1-"1");       // 0     Number
+
+// + : Arithmetic and Concatenation
+// - : Arithmetic 
+// / : Arithmetic
+// * : Arithmetic
+// % : Arithmetic
+
+// let age = Number(prompt("Enter Age"));
+// console.log(age);
+
+// Swapping no by 3 methods
+
+// 1.
+// let a = 10;
+// let b = 20;
+// console.log(a,b);
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a,b);
+
+//2.
+// let a = 10;
+// let b = 20;
+// console.log(a,b);
+
+// a = a + b;  a = 30
+// b = a - b;  b = 10
+// a = a - b;   a = 20
+// console.log(a,b);
+
+//3.
+// let a = 10;
+// let b = 20;
+// console.log(a,b);
+
+// [a,b] = [b,a];
+// console.log(a,b);
+
+
+// Operators
+
+// Arithmetic
+// console.log(10/3);                         3.3333333333333335
+// console.log(Math.floor(10/3));             3
+// console.log(10%3);                         1 
+
+
+// Relational
+// console.log(10>6);          true
+// console.log(10<6);          false
+// console.log(10<=10);        true
+// console.log(10==10);        true
+// console.log(10=="10");      true
+// console.log(10==="10");     false
+// console.log(10!=10);        false
+// console.log(10!=6);         true
+
+
+// Logical
+// console.log(10>8 && 15>5 && 9>4);      true
+// console.log(10>8 && 15<5 && 9>4);      false
