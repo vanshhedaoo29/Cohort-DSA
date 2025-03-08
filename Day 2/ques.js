97c4e5b3bba4dc89fa2dda6d23efa8fe4a728678
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-08 19:53:32 +0530

    Day 2 questions...

---
+++ Day 2/ques.js
@@ -0,0 +1,7 @@
+// Q. Accept two numbers and print the greatest between them
+// Q. Accept an integer and check whether it is an even number or odd
+// Q. Accept name and age from the user. Check if the user is a valid voted or not
+// Q. Accept three numbers and print the greatest among them
+// Q. Accept a year and check if it is a leap year or not
+// Q. Shop discount
+// Q. Electricity Bill
