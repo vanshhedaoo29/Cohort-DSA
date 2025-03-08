1ec99477cf330ebad498f13bac9bfd0a94d40ea4
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-08 15:34:40 +0530

    Day 2 in progress...

---
+++ Day 2/script.js
@@ -0,0 +1,17 @@
+console.log(Math.ceil(10.1));       // 11  Brings to higher range
+console.log(Math.floor(10.1));      // 10  Brings to lower range
+console.log(Math.round(10.49));     // 10  Rounded to nearest integer
+console.log(Math.round(10.5));       // 11 Rounded to nearest integer
+console.log(Math.abs(-8));       // 8 Returns the absolute value
+console.log(Math.abs(8));       // 8 Returns the absolute value
+console.log(Math.trunc(15.54946564446)); // 15  Returns the interger part
+console.log(Math.pow(5,2));
+console.log(Math.sqrt(36));
+console.log(Math.cbrt(125));
+console.log(Math.max(36,56,12,45));
+console.log(Math.min(36,56,12,45));
+console.log(Math.random());             // 0to 1
+
+let a = 234.984964561;
+let b = Number((a.toFixed(2)));
+console.log(b);
