6ed759ae29d0a3dd0b7c6fbb6e592809ec012f28
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-15 10:32:59 +0530

    Day 2 Completed Final

--- Day 2/ques.js
+++ Day 2/ques.js
@@ -1,7 +1,39 @@
-// Q. Accept two numbers and print the greatest between them
-// Q. Accept an integer and check whether it is an even number or odd
-// Q. Accept name and age from the user. Check if the user is a valid voted or not
-// Q. Accept three numbers and print the greatest among them
-// Q. Accept a year and check if it is a leap year or not
-// Q. Shop discount
-// Q. Electricity Bill
+// Q1. Accept two numbers and print the greatest between them
+
+
+// let a = Number(prompt("Enter Number 1 "));
+// let b = Number(prompt("Enter Number 2 "));
+// if(a>b){
+//     console.log(a + " is the greatest");
+// }
+// else{
+//     console.log(b + " is the greatest");
+// }
+
+
+// Q2. Accept an integer and check whether it is an even number or odd
+
+
+// let c = Number(prompt("Enter a Number"));
+// if(c % 2 == 0)console.log("Even");
+// else console.log("Odd");
+
+
+// Q3. Accept name and age from the user. Check if the user is a valid voted or not
+
+
+// let age = Number(prompt("Enter Age"));
+// let name = prompt("Enter Name");
+// if(age >= 18) console.log(name + " is a valid voter");
+// else console.log(name + " is not a valid voter");
+
+
+// Q4. Accept three numbers and print the greatest among them
+
+
+// let a = Number(prompt("Enter Number 1 "));
+// let b = Number(prompt("Enter Number 2 "));
+// let c = Number(prompt("Enter Number 3 "));
+// if(a>b && a>c) console.log(a + " is the greatest number");
+// else if(b>a && b>c) console.log(b + " is the greatest number");
+// else console.log(c + " is the greatest number");
