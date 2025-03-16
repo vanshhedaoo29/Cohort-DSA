ddf1a3da03957d4beab89431d2debf549eca3ec5
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-16 15:19:34 +0530

    Day 4 in Progress

---
+++ Day 4/ques.js
@@ -0,0 +1,29 @@
+// Q1. Accept an integer and print hello world n times 
+
+
+let num = Number(prompt("Enter a Number"))
+let i;
+if(isNaN(num))
+{
+    console.log("Invalid Input");
+}
+else
+{
+    for(i=1 ; i<=num ; i++)
+        {
+            console.log("Hello World !");
+        }
+        console.log('Fail at ' + i);
+}
+    
+
+
+// Q2. Print natural number upto n 
+// Q3. Reverse for loop, print n to 1 
+// Q4. Take a number as input and print its table 
+// Q5. Sum upto n terms
+// Q6. Factorial of a number 
+// Q7. Print the sum of all even and odd numbers in a range separately 
+// Q8. Print all the factors of a number 
+// Q9. Check if a number is prime or not 
+// Q10. WAP to take two inputs a and b and find the value of a raised to the power b  
