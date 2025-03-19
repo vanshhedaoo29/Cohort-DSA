5d5b536ce4c5c14ca6470e570e8cc52d99092e98
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-19 21:57:42 +0530

    DSA Day 9 Completed

---
+++ Day 9/ques.js
@@ -0,0 +1,23 @@
+/* WAP for finding out strong number or not*/
+
+// let prompt = require("prompt-sync")()
+// let n = Number(prompt("Enter a Number"))
+// let copy = n
+
+// let ans = 0;
+// whilw(n>0)
+// {
+//     let dig = n % 10;
+//     let fact = 1;
+//     for(let i =1; i<=dig ; i++)
+//     {
+//         fact *= i;
+//     }
+
+//     ans += fact
+//     n = Math.floor(n/10)
+// }
+// if(copy==ans) console.log("Strong Number");
+// else console.log("No Strong Number");
+
+
